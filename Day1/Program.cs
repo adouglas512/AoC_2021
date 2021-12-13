0241f1e8c37948d211753dca6e514b0546137985
adouglas512 <douglas.aaron@gmail.com>
2021-12-13 07:43:13 -0500

    Day 3.0 complete.

---
+++ Day1/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.IO;
+
+Console.WriteLine("Hello, World!");
+
+string[] input = File.ReadAllLines(@"Day1Input.txt");
+
+int counter = 0;
+
+//for (int i = 0; i < input.Length - 1; i++)
+//{
+//    if (int.Parse(input[i + 1]) > int.Parse(input[i])) counter++;    
+//}
+
+int windowA = 0;
+int windowB = 0;
+
+for (int i = 0; i < input.Length - 3; i++)
+{
+    for (int j = 0; j < 3; j++)
+    {
+        windowA += int.Parse(input[i + j]);
+        windowB += int.Parse(input[(i + 1) + j]);
+    }
+
+    Console.WriteLine("{0} : {1}", windowA, windowB);
+
+    if (windowB > windowA) counter++;
+
+    windowA = 0;
+    windowB = 0;
+}
+
+Console.WriteLine(counter);
---
+++ Day2/Program.cs
@@ -0,0 +1,42 @@
+﻿List<Command> Commands = new List<Command>();
+int horizontalPosition = 0;
+int verticalPosition = 0;
+int aim = 0;
+int finalPosition = 0;
+
+using (StreamReader sr = new StreamReader(@"Day2Input.txt"))
+{
+    string line = "";
+
+    while ((line = sr.ReadLine()) != null)
+    {
+        string[] parts = line.Split(new char[] { ' ' });
+        Commands.Add(new Command() { Direction = parts[0], Distance = int.Parse(parts[1]) });
+    }
+
+    foreach (Command command in Commands)
+    {
+        switch (command.Direction)
+        {
+            case "forward":
+                horizontalPosition += command.Distance;
+                verticalPosition += command.Distance * aim;
+                break;
+            case "down":
+                aim += command.Distance;
+                break;
+            case "up":
+                aim -= command.Distance;
+                break;
+        }
+    }
+
+    finalPosition = horizontalPosition * verticalPosition;
+    Console.WriteLine($"Horizontal: {horizontalPosition}  Vertitcal: {verticalPosition}  Final: {finalPosition}");
+}
+
+public class Command
+{
+    public string Direction { get; set; } = "";
+    public int Distance { get; set; } = 0;
+}
